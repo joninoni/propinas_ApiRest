25f10b86dd211fb81ccf7b1258e74a45f6ef0da3
joninoni <jonibeltranhayer@gmail.com>
2024-01-18 23:49:38 -0600

    mostrando la informacion de cada platillo en pantalla

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
     pedido:[],
 }
 
+const categoriasObj={
+    1:"Comida",
+    2:"Bebidas",
+    3:"Postres",
+}
+
 const btnGuardarCliente=document.querySelector("#guardar-cliente");
 btnGuardarCliente.addEventListener("click",guardarCliente);
 
@@ -42,8 +48,44 @@ function obtenerPlatillos(){
     const url=`http://localhost:4000/platillos`;
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado))
+        .then(resultado => mostrarPlatillos(resultado))
         .catch( error => console.log(error))
+}
+
+function mostrarPlatillos(platillos){
+    const contenido = document.querySelector("#platillos .contenido");
+    platillos.forEach( platillo => {
+        const {nombre,precio,id,categoria,} = platillo;
+        const row = document.createElement("div");
+        row.classList.add("row","py-3","border-top");
+
+        const titulo = document.createElement("div");
+        titulo.classList.add("col-md-4");
+        titulo.textContent = nombre;
+
+        const price = document.createElement("div");
+        price.classList.add("fw-bold","col-md-3");
+        price.textContent=`$${precio}`;
+
+        const category= document.createElement("div");
+        category.classList.add("col-md-4");
+        category.textContent= categoriasObj[categoria];//asignamos de forma dinamica la categoria ejemplo
+        //en el objecto global categoriasObj pusimos como llaves 1,2,3 por lo tanto la llave que sea igual
+        //al numero de categoria que tiene cada platillo en db.json le asignara el valor correspondiente a esa
+        //llave 
+        //ejemplo:
+        //const categoriasObj={
+        //     1:"Comida",
+        //     2:"Bebidas",
+        //     3:"Postres",
+        // }
+        //si la categoria es 1 sera Comida;
+        row.appendChild(titulo);
+        row.appendChild(price);
+        row.appendChild(category);
+
+        contenido.appendChild(row);
+    })
 }
 
 function mostrarAlerta(mensaje){
